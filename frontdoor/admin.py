fc63bf1dfe492f7ad4cc43f7f8be6a659e381cf3
Jason Shive <jshive@iot.in.gov>
2017-08-08 13:43:54 -0400

    testing

--- frontdoor/admin.py
+++ frontdoor/admin.py
@@ -5,6 +5,10 @@ from .models import RequestedParses, SiteSettings, ProcessedToons
 class RequestedParsesAdmin(admin.ModelAdmin):
     list_display = ('requesting_member', 'requesting_member_realm', 'created_at')
 
+class ProcessedToonsAdmin(admin.ModelAdmin):
+    list_display = ('character_name', 'character_race', 'character_faction', 'character_server', 'character_class', 'character_spec', 'character_level', 'character_equipped_ilevel')
+
+
 admin.site.register(RequestedParses, RequestedParsesAdmin)
 admin.site.register(SiteSettings)
-admin.site.register(ProcessedToons)
+admin.site.register(ProcessedToons, ProcessedToonsAdmin)
