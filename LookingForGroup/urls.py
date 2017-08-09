b8ea5dbf9f6a9177db0cada1a98cfd0dfaa02002
Jason Shive <jshive@iot.in.gov>
2017-08-09 13:02:26 -0400

    testing

--- LookingForGroup/urls.py
+++ LookingForGroup/urls.py
@@ -21,7 +21,7 @@ from frontdoor.views import index, parse_group, show_parses
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 #    url(r'^accounts/', include('allauth.urls')),
-    url(r'^group/', parse_group),
+    url(r'^results/', parse_group),
     url(r'^show/', show_parses),
     url(r'^$', index),
 ]
