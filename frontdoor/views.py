e3054caaf74df2c8742af3dad0630de48ef39254
Jason Shive <jshive@iot.in.gov>
2017-08-09 11:23:01 -0400

    testing

--- frontdoor/views.py
+++ frontdoor/views.py
@@ -18,13 +18,14 @@ def parse_group(request):
 
 
     # Check if user is banned
-    try:
-        user_id = RequestedParses.objects.filter(requesting_member=json_data['request']['requesting-member-name'], requesting_member_realm=json_data['request']['requesting-member-realm']).first()
-        # Make unique the BanHammer table so I don't need .first on that
-        banned = BanHammer.objects.get(character_name=user_id.id).first()
-        return render(request, 'sorry.html', {'banned': banned, 'user_id': user_id})
-    except ObjectDoesNotExist:
-        pass
+    user_id = RequestedParses.objects.filter(requesting_member=json_data['request']['requesting-member-name'], requesting_member_realm=json_data['request']['requesting-member-realm']).first()
+    # Make unique the BanHammer table so I don't need .first on that
+    if user_id:
+        try:
+            banned = BanHammer.objects.get(character_name=user_id.id).first()
+            return render(request, 'sorry.html', {'banned': banned, 'user_id': user_id})
+        except ObjectDoesNotExist:
+            pass
 
 
 
