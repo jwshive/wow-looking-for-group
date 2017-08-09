3274a487b4a2806d4f9798ec9644c3fd56ff546a
Jason Shive <jshive@iot.in.gov>
2017-08-09 14:06:01 -0400

    testing

--- helpers/parse_json.py
+++ helpers/parse_json.py
@@ -7,8 +7,10 @@ ENCODING = 'utf-8'
 
 # Convert JSON to base64
 with open('group.json', 'r') as json_stuff:
-    test_data = str(json.loads(json_stuff.read())).encode()
+    test_data = json.dumps(json_stuff.read()).encode()
+    #test_data = str(json.loads(json_stuff.read())).encode()
 
+print(test_data)
 
 base64_bytes = b64encode(test_data)
 #print(base64_bytes)
@@ -19,9 +21,9 @@ print(base64_string)
 
 
 # DECODING BASE64
-#json_data = json.loads(b64decode(base64_string).decode(ENCODING).replace('\'', '"'))
+json_data = json.dumps(b64decode(base64_string).decode(ENCODING).replace('\'', ''))
 
 #requesting_member_name = json_data['request']['requesting-member-name']
 #requesting_member_realm = json_data['request']['requesting-member-realm'] 
-#
-#print(json_data)
+
+print(json_data)
